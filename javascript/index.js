9161b2f4a79bcc3e68ae70dea536658bbf7688db
Bryant Chacón <bryantneta@hotmail.com>
2025-11-03 16:08:34 -0600

    javascript post buttons

--- javascript/index.js
+++ javascript/index.js
@@ -26,7 +26,39 @@ async function fetchGuide() {
     Prism.highlightElement(guide);
 }
 
+function topButton() {
+    const btnTop = document.querySelector('.top-button');
+
+    btnTop.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+}
+
+function toggleWrap() {
+    const btn = document.querySelector('.toggle-wrap');
+    const btnIcon = document.querySelector('.fa-align-justify');
+    const guide = document.querySelector('.guide');
+
+    btn.addEventListener('click', () => {
+        const y = window.scrollY;
+        guide.classList.toggle('code-wrap');
+
+        if (btnIcon.classList.contains('fa-align-justify')) {
+            btnIcon.classList.replace('fa-align-justify', 'fa-align-left');
+        } else {
+            btnIcon.classList.replace('fa-align-left', 'fa-align-justify');
+        }
+
+        window.scrollTo(0, y);
+    });
+}
+
 document.addEventListener('DOMContentLoaded', async () => {
     await loadPrism();
     await fetchGuide();
+    toggleWrap();
+    topButton();
 });
